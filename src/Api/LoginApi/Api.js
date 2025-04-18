6d83a10961bb2d07a320a2d9548ac558bd05b1f6
sathiya <sathiya@herbet.agency>
2025-04-18 18:21:30 +0530

    issue fixed 4

--- src/Api/LoginApi/Api.js
+++ src/Api/LoginApi/Api.js
@@ -5,7 +5,11 @@ import { apiurl } from '../ApiUrl/apiurl';
 
     export const LoginApi=async(data)=>{
       try{  
-         const res= await axios.post(`${apiurl}/clients/api/login`, data)
+         const res= await axios.post(`${apiurl}/clients/api/login`, data,
+          {
+            withCredentials: true,
+          }
+         )
          return res
       }catch(err){
        
